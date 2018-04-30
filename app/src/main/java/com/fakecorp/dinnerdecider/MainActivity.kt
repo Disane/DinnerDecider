3b276cab127be74b3d1aef1bf26e752778633930
elias.t <elias.t@ikarus.at>
2018-04-30 14:40:20 +0200

    Nag screen
    Added an advertisement nag screen!

--- app/src/main/java/com/fakecorp/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/fakecorp/dinnerdecider/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.fakecorp.dinnerdecider
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify
+import android.util.Log
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity()
 {
+    private val TAG = MainActivity::class.java.name
     private val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "McDonalds", "Vapiano")
 
     override fun onCreate(savedInstanceState: Bundle?)
@@ -29,5 +37,35 @@ class MainActivity : AppCompatActivity()
                 println(foodList)
             }
         }
+
+        callAlertDialog()
+    }
+
+    private fun callAlertDialog()
+    {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setPositiveButton("Ok!", DialogInterface.OnClickListener{ _, _ ->
+            Log.d(TAG, "You clicked Ok!")
+            val dialog: AlertDialog = builder.create()
+            dialog.setTitle("Advertisement!")
+            val message = TextView(this)
+            message.textSize = 21f
+            val s = SpannableString("This is an advertisement! Please visit: google.com")
+            Linkify.addLinks(s, Linkify.WEB_URLS)
+            message.text = s
+            message.movementMethod = LinkMovementMethod.getInstance()
+            dialog.setView(message)
+            dialog.show()
+        })
+        val dialog: AlertDialog = builder.create()
+        dialog.setTitle("Test AlertDialog!")
+        val message = TextView(this)
+        message.textSize = 21f
+        val s = SpannableString("This is an Alert Dialog! Please visit: google.com")
+        Linkify.addLinks(s, Linkify.WEB_URLS)
+        message.text = s
+        message.movementMethod = LinkMovementMethod.getInstance()
+        dialog.setView(message)
+        dialog.show()
     }
 }
